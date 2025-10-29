db04f441532a7200ad79b028c7973e489542800a
MARodriguez21 <moises.rodriguez21420@gmail.com>
2025-10-28 20:17:06 -0500

    Data Type Q2: Declare and Initialize Variables

---
+++ datatype.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+//datatype.c file
+
+//main
+int main()
+{
+	//task 2: declare and initialize variables
+	int oneInt = 1;
+	float oneFloat = 3.14f;
+	double oneDouble = 5.789;
+	char oneChar = 'A';
+
+	//print them
+	printf("int: %d\n", oneInt);
+	printf("float: %f\n", oneFloat);
+	printf("double: %lf\n", oneDouble);
+	printf("char: %c\n", oneChar);
+
+	return 0;
+}
+
